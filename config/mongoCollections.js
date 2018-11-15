2a8db2a8d35610c4aa04022ebd4d45c66cdd5a1d
robjweiss <rweiss55@gmail.com>
2018-11-15 10:28:11 -0500

    Created project structure

---
+++ config/mongoCollections.js
@@ -0,0 +1,21 @@
+const dbConnection = require("./mongoConnection");
+
+/* Reference to a given collection */
+const getCollectionFn = collection => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+/* Collections */
+module.exports = {
+  users: getCollectionFn("users"),
+  reviews: getCollectionFn("reviews")
+};
